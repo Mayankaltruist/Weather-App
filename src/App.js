721ef69484b50ce7d2c3ecdc32714eb78e8f37b5
mat943 <madhukarmayank93@gmail.com>
2023-05-16 15:03:15 +0530

    create default Ui to search city and show city temp

--- src/App.js
+++ src/App.js
@@ -1,7 +1,31 @@
+import hotBg from "./assests/hot.jpg";
+import coldBg from "./assests/cold.jpg";
+
 function App() {
   return (
-    <div className="app">
-      <h1>Hello</h1>
+    <div className="app" style={{ backgroundImage: `url(${coldBg})` }}>
+      <div className="overlay">
+        <div className="container">
+          <div className="section section__inputs">
+            <input type="text" name="city" placeholder="Enter City" />
+            <button>°F</button>
+          </div>
+          <div className="section section__temperature">
+            <div className="icon">
+              <h3>Ranchi, IN</h3>
+              <img
+                src="https://openweathermap.org/img/wn/02d@2x.png"
+                alt="weatherIcon"
+              />
+              <h3>Cloudy</h3>
+            </div>
+            <div className="temperature">
+              <h1>34 °C</h1>
+            </div>
+          </div>
+          {/* bottom description */}
+        </div>
+      </div>
     </div>
   );
 }
